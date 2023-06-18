dc7653e8cc84b24a4df33878dd265d12a025af87
Jeff Lovell <jeff@lovellworks.com>
2023-06-17 21:43:58 -0400

    add adverbs, better support for plurals, refactor to helper functions, fixed a bug causing intermittent crashes

--- mixed-messages/app.js
+++ mixed-messages/app.js
@@ -1,7 +1,11 @@
+/* eslint-disable complexity */
+/* eslint-disable max-statements */
+/* eslint-disable max-lines-per-function */
+/* eslint-disable no-use-before-define */
 /* eslint-disable max-len */
-import { nounsList, animalsList, plantsList, placesList, elementsList } from "./modules/dictionaries/nouns.js";
+import { subjectsList, objectsList } from "./modules/dictionaries/nouns.js";
 import { adjectivesList } from "./modules/dictionaries/adjectives.js";
-import { actionsList } from "./modules/dictionaries/verbs.js";
+import { verbsList } from "./modules/dictionaries/verbs.js";
 import { prepositionsList } from "./modules/dictionaries/helpers.js";
 import * as Tests from "./modules/tests/tests.js";
 
@@ -10,34 +14,116 @@ Tests.startUpTest( Tests.testNouns, Tests.testAdjectives, Tests.testVerbs, Tests
 
 // randomize the word
 function randomWord(list) {
+  // generate a random number between 0 and the length of the list - 1
   const randomIndex = Math.floor(Math.random() * list.length);
   return list[randomIndex];
 }
 
-function displayWord(entry) {
-  return entry[0];
+function getWord(list, maxSyllables) {
+
+  // only use words with 3 syllables or less for lines 1 and 3
+  if (maxSyllables === 5) {
+    list = list.filter(word => word[1] < 4);
+    return randomWord(list);
+  }
+  // otherwise just return the random word
+  return randomWord(list);
 }
 
-function numSyllables(entry) {
-  return entry[1];
+function articleGenerator(noun) {
+
+  let article = "";
+  let vowels = "aeiou";
+
+  // generate 1 or 2 randomly
+  let picker = Math.floor(Math.random() * 2) + 1;
+
+  // decide whether to use 'a(n)' or 'the' for singular nouns
+  if (noun[2] === "singular") {
+    if (picker === 1) {
+      article = "a";
+      // adjust 'a' to 'an' in the case of a leading vowel
+      if (vowels.includes(noun[0][0])) {
+        article += "n";
+      }
+    } else {
+      article = "the";
+    }
+  }
+
+  // randomly decide whether to include a subject article for plural and collective nouns
+  if (noun[2] !== "singular") {
+    if (picker === 1) {
+      article = "the";
+    }
+  }
+  return article;
 }
 
+function nounMatchedVerb(noun, maxSyllables, totalSyllables) {
+  console.log(maxSyllables, totalSyllables);
+  // filter the list of verbs to only contain verbs which match the noun's collection type
+  let nounMatchedVerbs = verbsList.filter(verb => verb[2] === (noun[2] || "collective"));
 
-function lineOne() {
-  // the maximum number of syllables for line 1
-  const maxSyllables = 5;
+  // filter the list of verbs again to only contain verbs which fit in the available line syllable count
+  let validVerbs = nounMatchedVerbs.filter(verb => (verb[1] + totalSyllables) <= maxSyllables);
 
-  // the current number of syllables for line 1
+  // return a random verb that meets both criteria
+  return randomWord(validVerbs);
+}
+
+function generateLine(maxSyllables) {
+
+  // the current number of syllables for the generated line
   let totalSyllables = 0;
 
-  // get the subject of line one and add its syllable value to the line total
-  let subject = randomWord(nounsList);
+  // set the subject
+  let subject = getWord(subjectsList);
+
+  // add the subject's syllable value to the line value
   totalSyllables += subject[1];
-  
-  let action = 
+
+  // randomly generate an article for the subject
+  let subjectArticle = articleGenerator(subject);
+
+  // if there is a non-empty string value for subject article, add 1 syllable to the line count
+  if (subjectArticle !== " ") {
+    totalSyllables += 1;
+  }
+
+  // set the subject's verb
+  let verb = nounMatchedVerb(subject, maxSyllables, totalSyllables);
+
+  // add the verb's syllable count to the line count
+  totalSyllables += verb[1];
+
+  // while there are less syllables than the line requires. . .
+
+  // add a subject adjective
+
+  // add an adverb
+
+  // add a conjunction and object
+
+  // add an object adjective
+
+  // randomly pick which solution to use
 
 
 
+  // trim the interpolated string in case there is a blank character at the beginning
+  let line = `${subjectArticle} ${subject[0]} ${verb[0]} | totalSyllables: ${totalSyllables} | maxSyllables: ${maxSyllables}`.trim();
+
+  // capitalize the first letter of the string
+  let capitalize = line[0].toUpperCase();
+
+  // recombine the capitalized first letter with the rest of the string
+  line = capitalize + line.slice(1);
+
+  // return the completed line
+  return line;
 }
 
-// {$ descriptor} ${animal, plant, place, element} ${verb}
+console.log(generateLine(5));
+console.log(generateLine(7));
+console.log(generateLine(5));
